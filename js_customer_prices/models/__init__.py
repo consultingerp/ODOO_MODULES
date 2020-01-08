80cf7826028630ee60e3b6d6b3b759ccf28276e5
Jose <josemrozas@gmail.com>
2020-01-08 09:49:35 +0100

    Inicio

--- js_customer_prices/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import product
-from . import customer_price
-from . import sale
--- js_flipbook/static/src/js/codigo.js
+++
@@ -1,6 +0,0 @@
-odoo.define('js_flipbook', function (require) {    
-    "use strict";
-    $(document).ready(function(){
-        $("ul.nav li a[href$='.pdf']").attr("target", "_blank");
-    });
-});
